21db5b7a4895ca4a6872277acbd42ec750965333
Sara Joseph <sarajoseph.job@gmail.com>
2024-06-20 11:23:35 +0200

    task2

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,17 @@
+import { MouseEventHandler } from "react";
+
 interface TutorialProps {
   title: string;
   description: string;
+  nextStepFN: MouseEventHandler<HTMLButtonElement> | undefined;
 }
 
-export const Card = ({ title, description }: TutorialProps) => {
+export const Card = ({ title, description, nextStepFN }: TutorialProps) => {
 	return (
 		<>
 			<h1>{ title }</h1>
 			<p>{ description }</p>
+			<button onClick={nextStepFN}>Next</button>
 		</>
 		
 	)
