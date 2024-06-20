8a9bca97334d1108815917b59fba00691a1b73cd
Sara Joseph <sarajoseph.job@gmail.com>
2024-06-20 13:43:45 +0200

    task4

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
-# S5. Onboarding digital
 
-En aquest lliurament aplicarem els conceptes bàsics que has après de React en el següent cas pràctic: la creació d'un Onboarding digital. Aquesta aplicació dispondrà de dos botons per a avançar o retrocedir a través de les frases, amb l'acompanyament d'una imatge que es modificarà.
+ # Sprint 5 - Onboarding digital
+
+In this delivery, we will apply the basic concepts you have learned about React in the following practical case: creating a digital Onboarding. This application will have two buttons to move forward or backward through the phrases, with an accompanying image that will change.
+
+
+
+## Instructions
+
+Download repository  
+`git clone https://github.com/sarajoseph/sprint5.git`  
+  
+Update dependencies  
+`npm install`  
+  
+
+## Dependencies  
+
+Vite  
+Tailwind  
+Heroicons  
+
+
+## Notes
+
+Only the Tailwind classes that are used are loaded. When adding a new one, it must be added to the css file.  
+`npx tailwindcss -i ./src/css/tailwind-input.css -o ./src/css/tailwind-output.css --watch`
--- src/App.tsx
+++ src/App.tsx
@@ -10,19 +10,25 @@ export const App = () => {
       title: 'Dedica moltes hores',
       description: 'Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaras una millora ràpidament.',
       bgColor: '4da2a9',
-      imageUrl: './src/assets/time_management.svg'
+      imageUrl: './src/assets/time_management.svg',
+      prevButton: false,
+      nextButton: true
     },    
     {
       title: 'Programa projectes propies',
       description: 'Més val 10 hores treballant en projectes propis, que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge.',
       bgColor: 'd3d4d9',
-      imageUrl: './src/assets/programming.svg'
+      imageUrl: './src/assets/programming.svg',
+      prevButton: true,
+      nextButton: true
     },    
     {
       title: 'Procura descansar',
       description: 'Descansar bé i desconnectar són vitals. D\'aquesta manera reduiràs l\'estrès i l\'ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge.',
       bgColor: 'ffd167',
-      imageUrl: './src/assets/meditation.svg'
+      imageUrl: './src/assets/meditation.svg',
+      prevButton: true,
+      nextButton: false
     }
   ]
 
@@ -34,6 +40,12 @@ export const App = () => {
     }
   }
 
+  const prevStep = (): void => {
+    if(step > 0){
+      setStep(prev => prev - 1)
+    }
+  }
+
   return (
     <Card 
       title={tutorialData[step].title} 
@@ -41,6 +53,9 @@ export const App = () => {
       imageUrl={tutorialData[step].imageUrl} 
       bgColor={tutorialData[step].bgColor} 
       nextStepFN={nextStep}
+      prevStepFN={prevStep}
+      prevButton={tutorialData[step].prevButton}
+      nextButton={tutorialData[step].nextButton}
     />
   )
 }
